270a89bcf0eef67486509c2e43b1e882b7aceccb
Renan Araujo <renanmatheuscorreiaaraujo@gmail.com>
2023-09-18 18:37:47 +0100

    chore: add rettag script

---
+++ tool/rettag_v1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+currentBranch=$(git symbolic-ref --short -q HEAD)
+if [[ ! $currentBranch == "main" ]]; then
+ echo "Releasing is only supported on the main branch."
+ exit 1
+fi
+
+# Get new version
+new_version="$1";
+
+if [[ "$new_version" == "" ]]; then
+  echo "No new version supplied, please provide one"
+  exit 1
+fi
+
+git tag -d v1 && git tag v1 v$new_version && git push origin --delete v1 && git push origin v1
